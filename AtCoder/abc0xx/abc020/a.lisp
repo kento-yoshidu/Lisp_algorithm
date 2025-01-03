368b25ee9de450012ab7cfb29f786e787c05b374
kento <kento@toriwatari.bird>
2025-01-03 12:41:36 +0900

    🛎 ABC020 A

---
+++ AtCoder/abc0xx/abc020/a.lisp
@@ -0,0 +1,12 @@
+; https://atcoder.jp/contests/abc020/tasks/abc020_a
+
+(defun fn (q)
+    (if (= q 1)
+        "ABC"
+        "chokudai"))
+
+(print (fn 1))
+;=> ABC
+
+(print (fn 2))
+;=> chokudai
