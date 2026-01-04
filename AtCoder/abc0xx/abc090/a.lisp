ace7aee863167a9daebcb8017e5cc4216a1fdbe8
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2026-01-04 12:47:39 +0900

    🧌 Update

---
+++ AtCoder/abc0xx/abc090/a.lisp
@@ -0,0 +1,13 @@
+; https://atcoder.jp/contests/abc090/tasks/abc090_a
+
+(defun fn(c1 c2 c3)
+    (format t "~c~c~c"
+        (char c1 0)
+        (char c2 1)
+        (char c3 2)))
+
+(print (fn "ant" "obe" "rec"))
+;=> abc
+
+(print (fn "edu" "cat" "ion"))
+;=> ean
---
+++ atcoder/abc0xx/abc090/a.lisp
@@ -0,0 +1,10 @@
+; https://atcoder.jp/contests/abc090/tasks/abc090_a
+
+(defun fn(c1 c2 c3)
+    (format t "~c~c~c"
+        (char c1 0)
+        (char c2 1)
+        (char c3 2)))
+
+(print (fn "ant" "obe" "rec"))
+;=> abc
